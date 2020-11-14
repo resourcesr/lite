d41547afe9774f6d587407ec6c9fe62cdde78b92
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-14 17:58:43 +0500

    add login and update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from  models import  *
+import re
 
 app = Flask(__name__)
 
@@ -28,6 +29,25 @@ def res(course):
     r = resources()
     resource = r.getByCourseId(course)
     return render_template("resources.html", resource=resource)
+
+
+@app.route("/signup")
+def login():
+    error = request.args['error']
+    return render_template("login.html", error=error)
+
+
+@app.route('/signup/action', methods=['POST'])
+def action():
+    data = request.form.to_dict()
+    emailRegx = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if not re.search(emailRegx, data['email']):
+        return redirect("/signup?error=Email is not valid")
+    if data['password'] != data['confirm']:
+        return redirect("/signup?error=Password should be match")
+    if len(data['password']) < 6:
+        return redirect("/signup?error=Password should be at least six character long")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
